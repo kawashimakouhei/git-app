047b556728412d5823f4c314fd47d72fa10735bc
kawashimakouhei <kk.1989.0722@gmail.com>
2020-09-17 08:51:34 +0900

    postsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-　root 'post#index'
+   root 'posts#index'
 end
+
+
 
 
